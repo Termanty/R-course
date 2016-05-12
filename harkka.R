f546eb039dc59b8444beb4cad7ddd9e0e417629c
termanty <termanty@gmail.com>
2016-05-12 12:09:38 +0300

    1b and 2 ready

--- harkka.R
+++ harkka.R
@@ -29,17 +29,44 @@ boxplot(asty$tyyt_kulj, col='red')
 
 # c)
 sarakkeet = c(paste('K1A', 4:6, sep=''), paste('K2A', 2:6, sep=''))
-asty$tyytyvaisyys <- summamuuttuja(sarakkeet, data=asty, max_puuttuvat=5)
-summary(asty$tyytyvaisyys)
-hist(asty$tyytyvaisyys, col='red', main="Matkustustyytyväisyys")
+tyytyvaisyys <- summamuuttuja(sarakkeet, data=asty, max_puuttuvat=5)
+summary(tyytyvaisyys)
+hist(tyytyvaisyys, col='red', main="Matkustustyytyväisyys")
 
 
 # d)
+suosittelu = factor(cut(asty$T16, breaks=c(0,1,2)), labels=c('kyllä','ei'))
+median(tyytyvaisyys[suosittelu == 'kyllä'], na.rm=T)
+median(tyytyvaisyys[suosittelu == 'ei'], na.rm=T)
+boxplot(tyytyvaisyys[suosittelu == 'kyllä'], main="Suosittelevien tyytyväisyys")
+boxplot(tyytyvaisyys[suosittelu == 'ei'], main="Ei suosittelevien tyytyväisyys")
 
 
 ##### tehtävä 2. #####
 
+# a)
+alue = factor(cut(asty$ALUE, breaks=c(49,752,2010)), labels=c('Sisäinen','Seutu'))
+
+# b)
+tasma = asty$K1A4
+alue_X_tasma = table(tasma, alue)
+prop.table(alue_X_tasma,1)
+
+# c)
+mu_seutu = mean(tasma[alue == 'Seutu'], na.rm=T)
+t.test(tasma[alue == 'Sisäinen'], mu= mu_seutu)
+
+
 ##### tehtävä 3. #####
+
+# a)
+
+# b)
+
+# c)
+
+# d)
+
 
 ##### tehtävä 4. #####
 
